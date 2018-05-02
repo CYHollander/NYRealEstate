42fa510ffff2559c2a3f858f63a37a01eef3a65b
C. Y. Hollander <judah0@gmail.com>
2018-05-02 09:36:21 -0400

    selectable date

--- ui.R
+++ ui.R
@@ -1,9 +1,19 @@
+mindate=min(bronx3$SALE_DATE)
+maxdate=max(bronx3$SALE_DATE)
 dashboardPage( 
   dashboardHeader(title="N.Y.C. Housing Prices"), 
   dashboardSidebar(
     sidebarUserPanel("C. Y.", image = "Face.jpg"),
     selectizeInput(inputId="year",
                    label="Year",
-                   choices=(2003:2015))),
-  dashboardBody(leafletOutput("nyc"))
+                   choices=(2015))
+    
+    ),
+  dashboardBody(leafletOutput("nyc"),
+                sliderInput("dates",
+                            "Months:",
+                            min = mindate,
+                            max = maxdate,
+                            value = c(mindate,maxdate))
+                )
 )
